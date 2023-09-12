ce40af13b9216dd4cffd4543ef5f453f521b6d1a
João Felipe <65171379+brjoaof@users.noreply.github.com>
2023-09-12 21:56:34 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return(
+        <div style={{width: "100vw", height: '100vh', display: "flex", justifyContent: "center", alignItems: 'center'}}>
+            <h1 style={{fontSize: 36, textAlign: 'center'}}>Luiza, minha esposa,<br />TE AMO!<br />Se gostou dá uma risadinha! 🥰❤</h1>
+        </div>
+    )
+}
