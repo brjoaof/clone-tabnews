589108fb2ebfca21a62871936de4606125a98019
João Felipe <65171379+brjoaof@users.noreply.github.com>
2023-09-14 20:21:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de Aprendizagem do https://curso.dev - Clone do https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 export default function Home() {
-    return(
-        <div style={{width: "100vw", height: '100vh', display: "flex", justifyContent: "center", alignItems: 'center'}}>
-            <h1 style={{fontSize: 36, textAlign: 'center'}}>Luiza, minha esposa,<br />TE AMO!<br />Se gostou dá uma risadinha! 🥰❤</h1>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ fontSize: 36, textAlign: "center" }}>
+        Luiza, minha esposa,
+        <br />
+        TE AMO!
+        <br />
+        Se gostou dá uma risadinha! 🥰❤
+      </h1>
+    </div>
+  );
 }
